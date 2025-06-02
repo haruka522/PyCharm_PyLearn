8f3db04a86993a5bae0dc2760998a2b634e3a20c
haruka522 <okamiharuka522@gmail.com>
2025-06-02 21:04:02 +0630

    2/6/25 1st Commit PyLesson

---
+++ Day16 Lesson/day16_1_Gui.py
@@ -0,0 +1,12 @@
+import FreeSimpleGUI as fsg
+
+label = fsg.Text("Type in a to-do")
+input_box = fsg.InputText(tooltip="Enter todo")
+add_button = fsg.Button("Add")
+
+
+window = fsg.Window("My To-Do App", layout=[[label],  [input_box, add_button]])
+
+
+window.read()
+window.close()
