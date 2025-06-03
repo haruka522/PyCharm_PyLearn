4eb43d8be7359af24698a9d2aac4a113063a8720
haruka522 <okamiharuka522@gmail.com>
2025-06-04 02:15:25 +0630

    4/6/25 Day-17-2 Implement the Edit Button

--- Day17 Lesson/day17_2_gui.py
+++ Day17 Lesson/day17_2_gui.py
@@ -12,7 +12,7 @@ add_button = fsg.Button("Add")
 list_box = fsg.Listbox(values=functions.get_todos(), size=(40, 10), key='todos', enable_events=True)
 edit_button = fsg.Button("Edit")
 
-layout = [[label], [input_box, add_button], [ list_box , edit_button]]
+layout = [[label], [input_box, add_button], [list_box, edit_button]]
 
 window = fsg.Window('My To-Do App', layout,
                     font=('Helvetica', 15))
@@ -21,13 +21,28 @@ while True:
     event, values = window.read()
     print(event)
     print(values)
+    print(values['todos'])
 
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values['todo'].strip()+"\n"
+            new_todo = values['todo'].strip() + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            input_box.update(value="")
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'].strip() + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            # you can replace functions.get_todos() with todos
+            window['todos'].update(values=todos)
+            input_box.update(value="")
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case fsg.WIN_CLOSED:
             break
 
