6697ef67e405bfdc9b9f757772bb6cbb933a2114
haruka522 <okamiharuka522@gmail.com>
2025-06-06 01:39:03 +0630

    6/6/25 Day-19 Implement "Add new-todo item" feature

--- Day19 Lesson/day19_2.py
+++ Day19 Lesson/day19_2.py
@@ -2,6 +2,14 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+def add_todo():
+    new_todo = st.session_state["new_todo"].strip() + "\n"
+    print(new_todo)
+    if new_todo:
+        todos.append(new_todo)
+        functions.write_todos(todos)
+        st.session_state["new_todo"] = ""
+
 
 st.title("My Todo App")
 st.subheader("This is my todo App")
@@ -10,4 +18,6 @@ st.write("lorem ipsum dolor sit amet, consectetur adipiscing elit.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input("Enter a new todo: ", placeholder="Type here...", key="new_todo")
+st.text_input(label="Enter a new todo: ", placeholder="Type here...", key="new_todo", on_change=add_todo)
+
+st.session_state
